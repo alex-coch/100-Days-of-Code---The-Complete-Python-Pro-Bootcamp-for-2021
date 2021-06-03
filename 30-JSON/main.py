6aea695ea011a4a390660e07c6ab8c046b2bafd0
alex-coch <a@deonop.com>
2021-06-03 14:30:50 +0400

    day31-TKinter

--- 30-JSON/main.py
+++ 30-JSON/main.py
@@ -1,27 +1,18 @@
-#Exception Handling
+import pandas
 
-try:
-    file = open("a_file.txt")
-    a_dictionary = {"key": "value"}
-    print(a_dictionary["key"])
-except FileNotFoundError:
-    file = open("a_file.txt", "w")
-    file.write("Something")
-except KeyError as error_message:
-    print(f"The key {error_message} does not exist.")
-else:
-    content = file.read()
-    print(content)
-finally:
-    # raise TypeError("This is an error that I made up.")
-    pass
-#BMI Example
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-height = float(input("Height: "))
-weight = int(input("Weight: "))
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
 
-if height > 3:
-    raise ValueError("Human Height should not be over 3 meters.")
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
 
-bmi = weight / height ** 2
-print(bmi)
+generate_phonetic()
