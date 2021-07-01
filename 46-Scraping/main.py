3e02ff40edd302ee8d5bbf304f4f02ad85dd2c58
alex-coch <a@deonop.com>
2021-07-01 10:22:43 +0400

    46-Scraping2

--- 46-Scraping/main.py
+++ 46-Scraping/main.py
@@ -1,21 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import mypass
 
+# Scraping Billboard 100
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+response = requests.get("https://www.billboard.com/charts/hot-100/" + date)
+soup = BeautifulSoup(response.text, 'html.parser')
+song_names_spans = soup.find_all("span", class_="chart-element__information__song")
+song_names = [song.getText() for song in song_names_spans]
+
+#Spotify Authentication
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
         scope="playlist-modify-private",
         redirect_uri="http://example.com",
-        client_id="1da2a06e7f61487bbed4df8813832005",
-        client_secret="4781f347ab1649018e9d0c756c98e23d",
+        client_id=CID,
+        client_secret=CS,
         show_dialog=True,
         cache_path="token.txt"
     )
 )
-
 user_id = sp.current_user()["id"]
-date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
-song_names = ["The list of song", "titles from your", "web scrape"]
+print(user_id)
 
+#Searching Spotify for songs by title
 song_uris = []
 year = date.split("-")[0]
 for song in song_names:
@@ -26,3 +36,10 @@ for song in song_names:
         song_uris.append(uri)
     except IndexError:
         print(f"{song} doesn't exist in Spotify. Skipped.")
+
+#Creating a new private playlist in Spotify
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+print(playlist)
+
+#Adding songs found into the new playlist
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
