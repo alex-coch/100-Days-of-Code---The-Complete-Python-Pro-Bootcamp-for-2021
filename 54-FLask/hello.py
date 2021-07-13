918c69fd988d9142f916d3a048e1b21fdada61fb
alex-coch <a@deonop.com>
2021-07-13 10:44:03 +0400

    55-HTML & URL Parsing in Flask

--- 54-FLask/hello.py
+++ 54-FLask/hello.py
@@ -1,6 +1,14 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route("/username/<name>")
+def say_bye():
+    return "Hello {name}"
+
+if __name__ == "__main__":
+    app.run()
