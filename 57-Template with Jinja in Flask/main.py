20d943641d5a5fd347a97727486a8b2ada77799b
alex-coch <a@deonop.com>
2021-07-19 08:48:54 +0400

    57-Template with Jinja in Flask.2

--- 57-Template with Jinja in Flask/main.py
+++ 57-Template with Jinja in Flask/main.py
@@ -3,23 +3,38 @@ import random
 import datetime
 import requests
 
-app = Flask(__name__, template_folder='template')
+app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    # return "Hello World!"
-    random_data = random.randint(1, 10)
+    random_number = random.randint(1, 10)
     current_year = datetime.datetime.now().year
-    return render_template("index.html", num=random_data, year=current_year)
+    return render_template("index.html", num=random_number, year=current_year)
 
-@app.route('/guess/<username>')
-def guess(username):
-    response = requests.get('https://api.genderize.io/?name='+username)
-    response_data = response.json()
-    response = requests.get('https://api.agify.io?name='+username)
-    response_data2 = response.json()
-    return render_template("guess.html", person_name=username, gender=response_data['gender'], age=response_data2['age'])
+@app.route("/guess/<name>")
+def guess(name):
+    gender_url = f"https://api.genderize.io?name={name}"
+    gender_response = requests.get(gender_url)
+    gender_data = gender_response.json()
+    gender = gender_data["gender"]
+    age_url = f"https://api.agify.io?name={name}"
+    age_response = requests.get(age_url)
+    age_data = age_response.json()
+    age = age_data["age"]
+    return render_template("guess.html", person_name=name, gender=gender, age=age)
+
+
+@app.route("/blog/<num>")
+def get_blog(num):
+    # print(num)
+    # blog_url = "https://api.npoint.io/5abcca6f4e39b4955965"
+    blog_url = "https://api.npoint.io/272a62adb47eb34a06b4"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    # print(all_posts)
+    return render_template("blog.html", posts=all_posts)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
